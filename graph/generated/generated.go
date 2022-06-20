ffb25cdd9c0918db439daa03f03b4037b93648f0
Warren Snipes <contact@warrensnipes.dev>
2022-06-20 02:47:09 -0400

    regenerated gqlgen gen files for latest version

--- graph/generated/generated.go
+++ graph/generated/generated.go
@@ -297,7 +297,7 @@ func (ec *executionContext) introspectType(name string) (*introspection.Type, er
 }
 
 var sources = []*ast.Source{
-	{Name: "graph/schema.graphqls", Input: `scalar Time
+	{Name: "../schema.graphqls", Input: `scalar Time
 
 type Event @key(fields: "id") {
   id: ID!
@@ -334,7 +334,7 @@ type Mutation {
   deleteEvent(id: ID!): Event!
 }
 `, BuiltIn: false},
-	{Name: "federation/directives.graphql", Input: `
+	{Name: "../../federation/directives.graphql", Input: `
 	scalar _Any
 	scalar _FieldSet
 	
@@ -345,7 +345,7 @@ type Mutation {
 
 	directive @key(fields: _FieldSet!) repeatable on OBJECT | INTERFACE
 `, BuiltIn: true},
-	{Name: "federation/entity.graphql", Input: `
+	{Name: "../../federation/entity.graphql", Input: `
 # a union of all types that use the @key directive
 union _Entity = Event
 
